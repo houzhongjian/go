4a48efb89504b2678670126982f65f128905c0e6
houzhongjian <1556469348@qq.com>
2016-10-27 10:01:41 +0800

    提交model

--- src/model/UserModel.go
+++ src/model/UserModel.go
@@ -2,13 +2,43 @@ package model
 
 import (
 	"db"
+	//	"fmt"
 )
 
-func AddUser(data map[string]string) bool {
+func AddUser(data map[string]string) (bool, map[string]string) {
 
 	//获取传递进来的map.
 	//将map里面的数据转换成sql.
-	strSql := "INSERT INTO `user`(`id`, `username`, `password`) VALUES (NULL,'" + data["username"] + "',md5(" + data["password"] + "))"
-	return db.Query(strSql)
+	errMap := make(map[string]string)
 
+	//	fmt.Println(data)
+
+	for k, v := range data {
+		if v == "" {
+			errMap[k] = UserMapFunc(k) + "不能为空"
+		}
+	}
+
+	if data["password"] != data["rePassword"] {
+		errMap["password"] = "两次密码不相同"
+	}
+
+	if len(errMap) > 0 {
+		return false, errMap
+	}
+
+	strSql := "INSERT INTO `user`(`id`, `username`, `password`) VALUES (NULL,'" + data["username"] + "',md5('" + data["password"] + "'))"
+	return db.Query(strSql), errMap
+}
+
+//map.
+func UserMapFunc(str string) string {
+
+	UserMap := make(map[string]string)
+
+	UserMap["username"] = "用户名"
+	UserMap["password"] = "密码"
+	UserMap["rePassword"] = "重复密码"
+
+	return UserMap[str]
 }
