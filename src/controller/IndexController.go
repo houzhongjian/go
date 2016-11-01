a1e8535fc7ef367b049ab10420468ee65e4f8f0a
houzhongjian <1556469348@qq.com>
2016-11-01 18:59:10 +0800

    提交

--- src/controller/IndexController.go
+++ src/controller/IndexController.go
@@ -10,10 +10,11 @@ type IndexController struct {
 
 //定义路径常量.
 const IndexPath = "../template/html/index/"
+const Layout = "../template/html/layout/"
 
 //首页方法.
 func (this *IndexController) IndexAction(resp http.ResponseWriter, req *http.Request) {
-
-	t, _ := template.ParseFiles(IndexPath + "index.html")
-	t.Execute(resp, nil)
+	t, _ := template.ParseFiles(Layout+"index.html", IndexPath+"right.html")
+	value := map[string]string{"username": "侯忠建"}
+	t.Execute(resp, value)
 }
